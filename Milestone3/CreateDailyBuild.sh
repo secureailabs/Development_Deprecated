290fb032211bd6c2b2ae114a9acc12eba135e1de
Prawal Gangwar <prawal.gangwar@gmail.com>
2021-08-30 18:07:03 +0530

    build script fi

--- Milestone3/CreateDailyBuild.sh
+++ Milestone3/CreateDailyBuild.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-#set -e 
-set -x 
+#set -e
+set -x
 
 foldername=$(date +'%Y-%m-%d')
 
@@ -23,27 +23,35 @@ cd WebService
 for d in $(echo ./*/); do (cd $d; make all -j; cd ..); done
 cd ..
 
+cd WebService/Plugins/RestApiPortal
+for d in $(echo ./*/); do (cd $d; make all -j; cd ..); done
+cd ../../..
+
+cd WebService/Plugins/DatabasePortal
+for d in $(echo ./*/); do (cd $d; make all -j; cd ..); done
+cd ../../..
+
 set -e
 
 cd InternalTools/PackageWebServiceAndComputeVm/
-make all -j 
+make all -j
 cd ../..
 
 # Copy more stuff into the Binary folder
-cp -r WebService/SharedLibraries/ Binary/
+# cp -r WebService/SharedLibraries/ Binary/
 cp VirtualMachine/DataConnectorPythonModule/libDataConnector.so Binary/
 
 cd Binary
 ./PackageWebServiceAndComputeVm
 cd ..
 
-# Create folder in the daily build for each 
+# Create folder in the daily build for each
 mkdir $foldername/SecureVmDeployment
 cp InternalTools/WindowsPlatformDeliverables/Binaries/x64/Debug/* $foldername/SecureVmDeployment/
 cp Binary/*.binaries $foldername/SecureVmDeployment/
 
 mkdir $foldername/DataSetSpecificationTool
-cp EndPointTools/DataSetSpecification/Release_v0.1/* $foldername/DataSetSpecificationTool 
+cp EndPointTools/DataSetSpecification/Release_v0.1/* $foldername/DataSetSpecificationTool
 
 mkdir $foldername/Orchestrator
 cp -r EndPointTools/ResearcherInterface/demo $foldername/Orchestrator/
@@ -51,4 +59,3 @@ cp -r EndPointTools/ResearcherInterface/sail $foldername/Orchestrator/
 
 mkdir $foldername/AuditViewer
 cp InternalTools/SailAuditViewer/Binaries/x64/Debug/* $foldername/AuditViewer
-

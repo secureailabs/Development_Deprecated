a30623682e03b56e247ef3fefbec6ebe92ff0643
Prawal Gangwar <prawal.gangwar@gmail.com>
2021-09-13 21:34:12 +0530

    DailyBuild to be usable in jenkins

--- Milestone3/CreateDailyBuild.sh
+++ Milestone3/CreateDailyBuild.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
-
-#set -e
+set -e
 set -x
-
-foldername=$(date +'%Y-%m-%d')
-
-rm -rf $foldername
-#rm -rf Binary
-mkdir $foldername
 
 #Build and put stuff in the Binary folder
 cd VirtualMachine
@@ -37,25 +30,10 @@ cd InternalTools/PackageWebServiceAndComputeVm/
 make all -j
 cd ../..
 
-# Copy more stuff into the Binary folder
-# cp -r WebService/SharedLibraries/ Binary/
-cp VirtualMachine/DataConnectorPythonModule/libDataConnector.so Binary/
+cd InternalTools/DatabaseTools/
+make all -j
+cd ../..
 
 cd Binary
 ./PackageWebServiceAndComputeVm
 cd ..
-
-# Create folder in the daily build for each
-mkdir $foldername/SecureVmDeployment
-cp InternalTools/WindowsPlatformDeliverables/Binaries/x64/Debug/* $foldername/SecureVmDeployment/
-cp Binary/*.binaries $foldername/SecureVmDeployment/
-
-mkdir $foldername/DataSetSpecificationTool
-cp EndPointTools/DataSetSpecification/Release_v0.1/* $foldername/DataSetSpecificationTool
-
-mkdir $foldername/Orchestrator
-cp -r EndPointTools/ResearcherInterface/demo $foldername/Orchestrator/
-cp -r EndPointTools/ResearcherInterface/sail $foldername/Orchestrator/
-
-mkdir $foldername/AuditViewer
-cp InternalTools/SailAuditViewer/Binaries/x64/Debug/* $foldername/AuditViewer

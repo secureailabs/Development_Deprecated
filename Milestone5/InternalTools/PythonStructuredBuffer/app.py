c5421fe71b282d5967699c15c6e58a44bbe14851
Prawal Gangwar <prawal.gangwar@gmail.com>
2021-11-17 03:00:09 +0530

    [BOARD-492] StructuredBuffer Python shared lib

---
+++ Milestone5/InternalTools/PythonStructuredBuffer/app.py
@@ -0,0 +1,19 @@
+from StructuredBuffer import StructuredBuffer
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+def GetJsonFromSerializedBuffer(serializedStructureBuffer):
+    oSb = StructuredBuffer(serializedBuffer=serializedStructureBuffer)
+    print(oSb.ToString())
+    strJson = oSb.ToJson()
+    return strJson
+
+@app.route('/StructuredBufferToJson', methods=['GET'])
+def StructuredBufferToJson():
+    return GetJsonFromSerializedBuffer(request.get_data())
+
+@app.route('/JsonToStructuredBuffer', methods=['GET'])
+def JsonToStructuredBuffer():
+    return 'Hello, Docker!'

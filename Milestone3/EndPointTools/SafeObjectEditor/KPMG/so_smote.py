288c28eba82f59f3cbd53b0cce0d3a6a7d0ee6d5
AdamHallSAIL <91956585+AdamHallSAIL@users.noreply.github.com>
2021-11-24 01:31:32 +0000

    BOARD-621 Safe Object version 4

--- Milestone3/EndPointTools/SafeObjectEditor/KPMG/so_smote.py
+++ Milestone3/EndPointTools/SafeObjectEditor/KPMG/so_smote.py
@@ -3,8 +3,8 @@ from imblearn.over_sampling import SMOTE
 from collections import Counter
 
 
-oversample = SMOTE(sampling_strategy=__sample_size, n_jobs=-1)
+oversample = SMOTE(sampling_strategy=__sampleSize, n_jobs=-1)
 print(Counter(__y))
 
-__X_sample, __y_sample = oversample.fit_resample(__X, __y)
-print(Counter(__y_sample_))
+__XSample, __ySample = oversample.fit_resample(__X, __y)
+print(Counter(__ySample_))
